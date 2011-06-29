623e2fd038adc7e9566dc4e1d801d531cbfeb32a
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2011-06-29 15:42:00 +0600

    first commit after create plugin and add export file

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+require 'redmine_export_pdf_patch' # overwrite base class
+
+Redmine::Plugin.register :redmine_greenmine_report do
+  name 'Redmine Greenmine Report plugin'
+  author 'Greenmine PMI'
+  description 'Plugin for manage PDF reports'
+  version '0.0.1'
+  url 'https://github.com/greenmine-pmi/redmine_greenmine_report'
+  author_url 'http://greenmine-pmi.blogspot.com/'
+end
